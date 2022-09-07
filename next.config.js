b231633a8dd1061dc4eb6f85c827bb082b2c0bea
brahiiim <brahim.abdelli994@gmail.com>
2022-09-08 00:10:35 +0200

    Adding backend url variable.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    LOCAL_APP_URL: 'http://localhost:3008/api',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
