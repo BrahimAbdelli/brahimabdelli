b4a4c60c7e8a19ce3b0ceb451c5c52fd6abe2567
brahiiim <brahim.abdelli994@gmail.com>
2022-10-03 16:10:29 +0200

    Adding global tailwind configurations.
    Including animations, color palettes and keyframes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/newline-after-import, import/no-extraneous-dependencies
+const colors = require('tailwindcss/colors');
 module.exports = {
+  darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+        homepageIcon: '#225da5',
+        accent: colors.fuchsia['400'],
+        'green-custom': '#64ffda',
+        'green-custom-highlighted': '#51a27f',
+      },
+    },
+    animation: {
+      type: 'type 2.7s ease-out .8s infinite alternate both',
+      animation: 'bounce 0.5s infinite',
+      ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+    },
+    keyframes: {
+      type: {
+        '0%': { transform: 'translateX(0ch)' },
+        '5%, 10%': { transform: 'translateX(1ch)' },
+        '15%, 20%': { transform: 'translateX(2ch)' },
+        '25%, 30%': { transform: 'translateX(3ch)' },
+        '35%, 40%': { transform: 'translateX(4ch)' },
+        '45%, 50%': { transform: 'translateX(5ch)' },
+        '55%, 60%': { transform: 'translateX(6ch)' },
+        '65%, 70%': { transform: 'translateX(7ch)' },
+        '75%, 80%': { transform: 'translateX(8ch)' },
+        '85%, 90%': { transform: 'translateX(9ch)' },
+        '95%, 100%': { transform: 'translateX(11ch)' },
+      },
+    },
   },
   plugins: [],
 };
