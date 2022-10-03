6ef73de3398242b392fa5330732c53737dad9bfb
brahiiim <brahim.abdelli994@gmail.com>
2022-10-03 16:10:49 +0200

    Global styles to be implemented throughout the project.

---
+++ src/components/styles.tsx
@@ -0,0 +1,14 @@
+export const containerClasses =
+  'py-12 lg:py-16 xl:py-24 2xl:py-32 px-6 md:px-12 lg:px-24 max-w-screen-2xl w-full mx-auto';
+export const containerClassesNoYPadding =
+  'px-6 md:px-12 lg:px-24 max-w-screen-2xl w-full mx-auto';
+export const sectionTitleBigClasses =
+  'text-4xl sm:text-5xl md:text-6xl xl:text-8xl text-white font-semibold lg:-ml-1 xl:-ml-1';
+export const sectionTitleClasses =
+  'text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-bold';
+export const sectionBodyClasses =
+  'text-slate-600 dark:text-slate-400 sm:text-lg lg:text-xl leading-relaxed max-w-prose space-y-4 sm:space-y-6 lg:space-y-8';
+export const linkClasses =
+  'flex flex-col items-start transform transition hover:-translate-y-1 text-slate-500 dark:text-slate-400 dark:hover:text-green-custom hover:text-accent border-b border-slate-400 hover:border-accent dark:text-green-custom';
+export const switchableTexts =
+  'dark:group-hover:text-slate-300 group-hover:text-slate-800 text-slate-900 dark:text-slate-400';
