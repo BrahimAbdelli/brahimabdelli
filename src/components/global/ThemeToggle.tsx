202ffc01ed001e97f806bd4f56ddc90f4d1bb8f5
brahiiim <brahim.abdelli994@gmail.com>
2022-10-03 16:08:06 +0200

    Creating Theme component and implementing it.

---
+++ src/components/global/ThemeToggle.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import { useTheme } from 'next-themes';
+import { BiMoon, BiSun } from 'react-icons/bi';
+
+export default function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  function isDark() {
+    return theme === 'dark';
+  }
+
+  return (
+    <button
+      className="focus:outline-none ml-4"
+      onClick={() => {
+        console.log(theme);
+        setTheme(isDark() ? 'light' : 'dark');
+        console.log(theme);
+      }}
+      aria-label="Theme toggle"
+    >
+      {isDark() ? <BiSun size={20} /> : <BiMoon size={20} />}
+    </button>
+  );
+}
