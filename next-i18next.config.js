d5eb0f419e8e64c7f6103774e9f95277f4642aee
brahiiim <brahim.abdelli994@gmail.com>
2022-10-15 00:38:44 +0200

    Updating i18n next dependency and changing configuration.

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  i18n: {
+    defaultNS: 'common',
+    ns: ['common'],
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+    localeDetection: false,
+  },
+};
--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
-const { nextI18NextRewrites } = require('next-i18next/rewrites');
-
-const localeSubpaths = { fr: 'fr', en: 'en' };
+const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n,
   reactStrictMode: true,
   swcMinify: true,
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
 };
 
 module.exports = nextConfig;
