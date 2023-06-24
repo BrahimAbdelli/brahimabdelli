2babf05b19f42d30cc4a0e3b57dbb7ef3ceb80e3
brahiiim <brahim.abdelli994@gmail.com>
2023-06-24 12:38:32 +0200

    Adding baseUrl config to tsconfig.

--- src/components/styles.tsx
+++ src/components/styles.tsx
@@ -1,11 +1,10 @@
 export const containerClasses =
   'py-12 lg:py-16 xl:py-24 2xl:py-32 px-6 md:px-12 lg:px-24 max-w-screen-2xl w-full mx-auto';
-export const containerClassesNoYPadding =
-  'px-6 md:px-12 lg:px-24 max-w-screen-2xl w-full mx-auto';
+export const containerClassesNoYPadding = 'px-6 md:px-12 lg:px-24 max-w-screen-2xl w-full mx-auto';
 export const sectionTitleBigClasses =
-  'text-4xl sm:text-5xl md:text-6xl xl:text-8xl text-white font-semibold lg:-ml-1 xl:-ml-1';
+  'text-4xl sm:text-5xl md:text-6xl xl:text-8xl font-semibold lg:-ml-1 xl:-ml-1 dark:text-white text-black';
 export const sectionTitleClasses =
-  'text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-bold';
+  'text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold dark:text-white text-black';
 export const sectionBodyClasses =
   'text-slate-600 dark:text-slate-400 sm:text-lg lg:text-xl leading-relaxed max-w-prose space-y-4 sm:space-y-6 lg:space-y-8';
 export const linkClasses =
