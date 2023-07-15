b3f04fc82a0cdc63e8b4efe1f2854efc357d3688
brahiiim <brahim.abdelli994@gmail.com>
2023-07-16 00:00:45 +0200

    Create index exporter.

---
+++ src/components/notion/index.ts
@@ -0,0 +1,30 @@
+export {
+  NotionHasChildrenRender,
+  NotionBlocksRender,
+  NotionCalloutBlock,
+  NotionChildDatabaseBlock,
+  NotionCodeBlock,
+  NotionColumnListBlock,
+  NotionCopyHeadingLink,
+  NotionHeadingBlock,
+  NotionImageBlock,
+  NotionLinkPreviewBlock,
+  NotionListBlock,
+  NotionPageHeader,
+  NotionParagraphBlock,
+  NotionParagraphText,
+  NotionQuoteBlock,
+  NotionSecureImage,
+  NotionSeo,
+  NotionTableBlock,
+  NotionTodoBlock,
+  NotionToggleBlock,
+  NotionVideoBlock,
+  NotionDatabasePageView,
+  NotionEquationBlock,
+  NotionFileBlock,
+  NotionSyncedBlock,
+  NotionTableOfContents
+} from './lib';
+
+export { NotionRender } from './NotionRender';
