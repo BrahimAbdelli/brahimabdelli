31c315c8d3a98973cf3d54afa8a38eefb850b0f7
brahiiim <brahim.abdelli994@gmail.com>
2023-07-16 00:00:27 +0200

    Adding usable modules.

---
+++ src/components/modules/OptionalNextLink.tsx
@@ -0,0 +1,18 @@
+import type React from 'react';
+
+import Link, { LinkProps } from 'next/link';
+
+export const OptionalNextLink: React.FC<
+  LinkProps &
+    React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+      wrappingAnchor: boolean;
+      children: JSX.Element;
+    }
+> = ({ wrappingAnchor, children, ...props }) => {
+  if (wrappingAnchor) {
+    return <Link {...props}>{children}</Link>;
+  }
+  const { scroll, shallow, prefetch, as, replace, passHref, ...spanProps } =
+    props;
+  return <span {...spanProps}>{children}</span>;
+};
